ba3a4e789866173c8e9ef865ecc30af9eae3b380
Niks Birzgalis <75217926+birzgalisn@users.noreply.github.com>
2023-01-03 15:02:51 +0200

    improve scrollable component logic

--- src/features/workspaces/components/Scrollable.tsx
+++ src/features/workspaces/components/Scrollable.tsx
@@ -4,8 +4,8 @@ export const Scrollable: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
   return (
-    <div className="relative -left-6 -right-6">
-      <div className="absolute flex w-screen gap-6 overflow-x-scroll pb-6 first:pl-6 last:pr-6">
+    <div className="relative -left-6 w-screen">
+      <div className="flex w-full gap-6 overflow-x-scroll pb-6 first:pl-6 last:pr-6">
         {children}
       </div>
     </div>
