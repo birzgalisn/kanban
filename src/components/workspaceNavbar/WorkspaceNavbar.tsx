8a6f3cb93e8cc57f84d2172a91946d445315f49f
Niks Birzgalis <75217926+birzgalisn@users.noreply.github.com>
2023-01-03 20:06:38 +0200

    make workspace navbar mobile friendly

--- src/components/workspaceNavbar/WorkspaceNavbar.tsx
+++ src/components/workspaceNavbar/WorkspaceNavbar.tsx
@@ -1,16 +1,18 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 import { Avatar } from "@/ui/avatar";
 import { Button } from "@/ui/button";
 import { VerticalDivider } from "@/ui/verticalDivider";
-import clsx from "clsx";
 import {
+  HiOutlineArrowUturnLeft,
   HiOutlineGlobeEuropeAfrica,
   HiOutlineServer,
   HiOutlineStar,
   HiOutlineUserPlus,
 } from "react-icons/hi2";
-import { AvatarSkeleton, TextSkeleton } from "../skeleton";
+import { AvatarGroupSkeleton, TextSkeleton } from "../skeleton";
 
 type Member = {
   __typename?: string;
@@ -26,16 +28,29 @@ export const WorkspaceNavbar: React.FC<{
   members?: Array<Member>;
   isLoading: boolean;
 }> = ({ title, members, isLoading }) => {
+  const router = useRouter();
+  const isInBoardPage = router.asPath.match("boards");
+  const workspaceId = router.query.workspaceId;
+
   return (
     <div className="flex h-16 w-full items-center border-b border-gray-200 bg-white px-6 py-2">
       <div className="flex w-full items-center justify-between">
         <div className="flex max-w-fit items-center gap-2">
+          {isInBoardPage && (
+            <Link href={`/workspaces/${workspaceId}`}>
+              <Button
+                variant="transparent"
+                size="sm"
+                startIcon={<HiOutlineArrowUturnLeft className="h-4 w-4" />}
+              />
+            </Link>
+          )}
           {isLoading ? (
             <TextSkeleton className="h-9 w-52" />
           ) : (
-            <h1 className="mr-12 text-3xl font-bold">{title}</h1>
+            <h1 className="truncate text-3xl font-bold md:mr-12">{title}</h1>
           )}
-          <div className="hidden max-w-fit items-center gap-2 xs:flex">
+          <div className="hidden max-w-fit items-center gap-2 lg:flex">
             <Button
               variant="transparent"
               size="sm"
@@ -62,28 +77,20 @@ export const WorkspaceNavbar: React.FC<{
           </div>
         </div>
       </div>
-      <div className="ml-6 flex items-center">
-        {isLoading
-          ? Array(5)
-              .fill(0)
-              .map((_, idx) => {
-                const isEven = idx % 2;
-                return (
-                  <AvatarSkeleton
-                    key={idx}
-                    className={clsx("-ml-3 h-8 w-8", isEven && "bg-gray-100")}
-                  />
-                );
-              })
-          : members &&
-            members.map((member) => (
-              <Avatar
-                className="-ml-3"
-                key={member.id}
-                src={member.user.image}
-                sizes="w-8 h-8"
-              />
-            ))}
+      <div className="hidden items-center xs:ml-6 xs:flex">
+        {isLoading ? (
+          <AvatarGroupSkeleton count={5} size="-ml-3 h-8 w-8" />
+        ) : (
+          members &&
+          members.map((member) => (
+            <Avatar
+              className="-ml-3"
+              key={member.id}
+              src={member.user.image}
+              size="w-8 h-8"
+            />
+          ))
+        )}
         <Button
           className="ml-4"
           size="sm"
