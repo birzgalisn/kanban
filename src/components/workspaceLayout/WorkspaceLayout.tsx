9014f0cfbdc95602ce8d59700266883911dd23af
Niks Birzgalis <75217926+birzgalisn@users.noreply.github.com>
2023-01-04 16:10:15 +0200

    refactor workspace layout component

--- src/components/workspaceLayout/WorkspaceLayout.tsx
+++ src/components/workspaceLayout/WorkspaceLayout.tsx
@@ -1,7 +1,23 @@
+import clsx from "clsx";
 import React from "react";
 
-export const WorkspaceLayout: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  return <div className="flex h-[calc(100vh-8rem)] w-full">{children}</div>;
+import { Layout } from "../layout";
+import { WorkspaceNavbar } from "../workspaceNavbar";
+
+export const WorkspaceLayout: React.FC<{
+  title?: string;
+  isLoading: boolean;
+  children: React.ReactNode;
+  noMargin?: boolean;
+}> = ({ title, isLoading, children, noMargin = false }) => {
+  return (
+    <Layout noMargin>
+      <WorkspaceNavbar title={title} isLoading={isLoading} />
+      <div
+        className={clsx("flex h-[calc(100vh-8rem)] w-full", !noMargin && "p-6")}
+      >
+        {children}
+      </div>
+    </Layout>
+  );
 };
