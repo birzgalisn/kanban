986fec786032e1aa68f41711f6ca65bcbb3e618e
Niks Birzgalis <75217926+birzgalisn@users.noreply.github.com>
2023-01-07 15:25:23 +0200

    hide overflow when not overflown

--- src/features/workspace/components/WorkspaceMenu.tsx
+++ src/features/workspace/components/WorkspaceMenu.tsx
@@ -22,7 +22,7 @@ export const WorkspaceMenu: React.FC<{
     <div className="flex min-w-fit max-w-xs flex-col gap-2 whitespace-normal border-r border-gray-200 bg-white p-6">
       <p className="w-fit text-sm font-semibold">Menu</p>
       <h1 className="w-52 font-bold">All boards ({boardCount})</h1>
-      <div className="flex h-full flex-col gap-2 overflow-y-scroll scroll-smooth">
+      <div className="flex h-full flex-col gap-2 overflow-hidden overflow-y-auto scroll-smooth">
         {isBoardsLoading
           ? Array(3)
               .fill(0)
--- src/features/workspaces/components/Scrollable.tsx
+++ src/features/workspaces/components/Scrollable.tsx
@@ -5,7 +5,7 @@ export const Scrollable: React.FC<{
 }> = ({ children }) => {
   return (
     <div className="relative -left-6 w-screen">
-      <div className="flex w-full gap-6 overflow-x-scroll pb-6 first:pl-6 last:pr-6">
+      <div className="flex w-full gap-6 overflow-hidden overflow-x-auto pb-6 first:pl-6 last:pr-6">
         {children}
       </div>
     </div>
