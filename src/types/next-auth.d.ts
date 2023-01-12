aa1cd5647aa44fa0cd2c67bf25e1a6b653dfa3d8
Niks Birzgalis <75217926+birzgalisn@users.noreply.github.com>
2023-01-13 00:36:11 +0200

    correctly extend next-auth default session type

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,14 +2,15 @@ import "next-auth";
 import "next-auth/jwt";
 
 import type { User as PrismaUser } from "@prisma/client";
+import type { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface User extends PrismaUser {}
 
-  interface Session {
+  interface Session extends PrismaUser {
     user: {
       id: JWT["sub"];
-    };
+    } & DefaultSession["user"];
   }
 }
 
