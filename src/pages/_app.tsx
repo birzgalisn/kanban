9631c8caf616661b117384051d6ee53d3c9b0772
Niks Birzgalis <75217926+birzgalisn@users.noreply.github.com>
2024-06-12 22:42:47 +0300

    Add speed insight metrics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/globals.css';
 import { useApollo } from '@/hooks/useApollo';
 import { ApolloProvider } from '@apollo/client';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { SessionProvider } from 'next-auth/react';
 
 import type { AppProps } from 'next/app';
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
       <ApolloProvider client={client}>
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </ApolloProvider>
     </SessionProvider>
   );
